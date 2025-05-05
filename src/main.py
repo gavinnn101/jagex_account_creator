8c2f373ccfc408a3a9c8ac0381b9f694f01666f9
gavinnn101 <41026935+gavinnn101@users.noreply.github.com>
2025-05-04 20:17:14 -0700

    bump user agent to current chrome

--- src/main.py
+++ src/main.py
@@ -137,7 +137,7 @@ class AccountCreator:
 
         # custom user-agent is only needed for headless but why not make it consistent.
         co.set_user_agent(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"
         )
 
         if self.headless:
