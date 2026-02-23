4f688748ee709864c6f9a88b29d40dde99b9c188
gavinnn101 <41026935+gavinnn101@users.noreply.github.com>
2026-02-23 13:07:34 -0800

    Include a `convert_accounts.py` helper script to convert `JagexAccount` objects to different formats for exporting.

---
+++ convert_accounts.py
@@ -0,0 +1,24 @@
+# Convert JagexAccount objects to different formats for account management on different platforms.
+
+from pathlib import Path
+
+from jagex_account_creator.models import JagexAccount
+
+SCRIPT_DIR = Path(__file__).resolve().parent
+
+ACCOUNTS_FILE_PATH = SCRIPT_DIR / "accounts.jsonl"
+CONVERTED_ACCOUNTS_FILE_PATH = SCRIPT_DIR / "converted_accounts.txt"
+
+
+def main():
+    with open(ACCOUNTS_FILE_PATH) as in_file, open(CONVERTED_ACCOUNTS_FILE_PATH, "w") as out_file:
+        for line in in_file:
+            account = JagexAccount.model_validate_json(line.strip())
+            acc_data = f"{account.email}:{account.password}"
+            if account.tfa:
+                acc_data += f":{account.tfa.setup_key}"
+            out_file.write(f"{acc_data}\n")
+
+
+if __name__ == "__main__":
+    main()
